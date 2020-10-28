a4a31307db4635d16fc9f3271cde3cc95de9049c
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-10-28 16:09:12 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// function Stateless(props) {
+//   return (
+//     <div>
+//     </div>
+//   );
+// }
+
+// const Stateless = props => {
+//   return (
+//     <div>
+//     </div>
+//   );
+// }
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+  
+  render() {  
+    return (
+      <div>
+        <h1>25 + 5 Clock</h1>
+      </div>
+    );
+  }
+}
+  
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Clock />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
